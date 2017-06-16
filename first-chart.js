41eb4e9b56f3f4cb7085b04bf0a43d353816ae02
kerry <keonei@uw.edu>
2017-06-15 20:22:05 -0700

    added chart

---
+++ first-chart.js
@@ -0,0 +1,33 @@
+// Load the Visualization API and the corechart package.
+  google.charts.load('current', {'packages':['corechart']});
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'People Who Like It');
+    data.addRows([
+      ['Tomato', 6],
+      ['Pepperoni', 7],
+      ['Peppers', 4],
+      ['Fried Shrimp', 1],
+      ['Pineapple', 6]
+    ]);
+
+    // Set chart options
+    var options = {'title':'Pizza Topping Popularity',
+                   'width':400,
+                   'height':300,
+                   pieHole: 0.4};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
